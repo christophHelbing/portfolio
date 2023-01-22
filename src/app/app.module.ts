36c2af2bfa4f883269dae14fc8dd8ec81d199b10
Christoph, Helbing <christoph.helbing@conet-isb.de>
2023-01-22 13:24:59 +0100

    Creating header component which will be the home part of the website. Adding license free background image. Adding text for header component and typed.js for the wrting effect.
    Creating about component which will contain my personal information.
    
    Refactoring navbar to ng-bootstrap navbar. Now the navbar toogle for mobile view is working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+import { HeaderComponent } from './components/header/header.component';
+import {NgxTypedJsModule} from "ngx-typed-js";
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HeaderComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    NgxTypedJsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
